d071f3c15686cdaaf3dddb86e973344cb7c9e2df
TatianaGoryashina <ichi.neko.in@gmail.com>
2025-07-05 14:53:10 +0800

    Выполнение заданий за лектором

---
+++ lesson_6/get_title.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+
+browser = webdriver.Firefox(
+    service=FirefoxService(GeckoDriverManager().install())
+    )
+
+browser.get("https://sky.pro/")
+current_title = browser.title
+print(current_title)
+
+browser.quit()
