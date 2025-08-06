316a9488229592057c1bee412d850fd9de3a0fa4
TatianaGoryashina <ichi.neko.in@gmail.com>
2025-08-06 16:07:40 +0800

    Изменила код, исправила под Pep8

--- lesson_9/database.py
+++ lesson_9/database.py
@@ -3,28 +3,37 @@ from sqlalchemy.sql import text
 
 
 class Subjects:
-        sql_st = {
-        "select": text("SELECT * FROM subject WHERE deleted_at IS NULL"),
-        "delete_by_id": text("DELETE FROM subject WHERE subject_id = :id_to_delete"),
-        "insert_new": text("INSERT INTO subject(\"subject_title\") values (:new_title)"),
-        "change": text("UPDATE subject SET subject_title = :new_title WHERE subject_id = :id"),
-        "get_max_id": text("SELECT MAX(\"subject_id\") FROM subject WHERE deleted_at IS NULL")
+    sql_st = {
+        "select": text("SELECT * FROM subject"),
+        "delete_by_id": text("DELETE FROM subject "
+                             "WHERE subject_id = :id_to_delete"),
+        "insert_new": text("INSERT INTO subject(subject_id, subject_title)"
+                           " values (200, :title)"),
+        "change": text("UPDATE subject SET subject_title = :new_title "
+                       "WHERE subject_id = :id"),
+        "get_max_id": text("SELECT MAX(subject_id) FROM subject"),
+        "get_subject": text("SELECT * FROM subject "
+                            "WHERE subject_title = :subject_title")
         }
 
-        def __init__(self, connection_string):
-                self.__db = create_engine(connection_string)
+    def __init__(self, connection_string):
+        self.__db = create_engine(connection_string)
 
-        def get_subjects(self):
-                return self.__db.execute(self.sql_st["select"]).fetchall()
+    def get_subjects(self):
+        return self.__db.execute(self.sql_st["select"]).fetchall()
 
-        def delete(self, subject_id):
-                self.__db.execute(self.sql_st["delete_by_id"], id_to_delete=subject_id)
+    def delete(self, subject_id):
+        self.__db.execute(self.sql_st["delete_by_id"], id_to_delete=subject_id)
 
-        def create(self, title):
-                self.__db.execute(self.sql_st["insert_new"], title=title)
+    def create(self, title):
+        self.__db.execute(self.sql_st["insert_new"], title=title)
 
-        def update(self, new_title):
-                self.__db.execute(self.sql_st["change"], new_title=new_title)
+    def update(self, new_title, id):
+        self.__db.execute(self.sql_st["change"], new_title=new_title, id=id)
 
-        def get_max_id(self):
-                return self.__db.execute(self.sql_st["get_max_id"]).fetchall()[0][0]
+    def get_max_id(self):
+        return self.__db.execute(self.sql_st["get_max_id"]).fetchall()[0][0]
+
+    def get_subject(self, title):
+        return self.__db.execute(self.sql_st["get_subject"],
+                                 subject_title=title).fetchall()[0]
--- lesson_9/test_database_operations.py
+++ lesson_9/test_database_operations.py
@@ -1,52 +1,46 @@
 from database import Subjects
-from sqlalchemy import create_engine
-import pytest
 
 
-db_connection_strings = ""
-db = create_engine(db_connection_strings)
+db_connection_strings = "postgresql://postgres:123@localhost:5432/postgres"
+sub = Subjects(db_connection_strings)
 
 
-def test_add_new(db):
-    sub = Subjects(db)
-    body = db.sub.get_subjects()
+def test_add_new():
+    body = sub.get_subjects()
     len_before = len(body)
 
     title = "Autotest"
-    result = sub.create(title)
-    new_id = result["subject_id"]
+    sub.create(title)
+    result = sub.get_subject(title)
 
     body = sub.get_subjects()
     len_after = len(body)
 
-    sub.delete(new_id)
+    sub.delete(result[0])
 
     assert len_after - len_before == 1
-    for title in body:
-            if title["subject_id"] == new_id:
-                assert title["subject_title"] == title
-                assert title["subject_id"] == new_id
+    assert result[0] == 200
+    assert result[1] == title
+
 
 def test_edit():
     title = "etiquette"
-    db.create(title)
-    max_id = db.get_max_id()
+    sub.create(title)
+    max_id = sub.get_max_id()
 
     new_title = "Updated_etiquette"
-    update = db.update(max_id, new_title)
+    sub.update(new_title, max_id)
+    result = sub.get_subject(new_title)
+    sub.delete(result[0])
+    assert result[1] == new_title
 
-    db.delete(max_id)
-
-    assert update["subject_title"] == new_title
 
 def test_delete():
     title = "etiquette"
-    db.create(title)
-    max_id = db.get_max_id()
+    sub.create(title)
+    max_id = sub.get_max_id()
 
-    deleted = db.delete(max_id)
+    sub.delete(max_id)
+    max_id = sub.get_max_id()
 
-    assert deleted["subject_id"] == max_id
-
-    rows = db.get_subject(max_id)
-    assert len(rows) == 0
+    assert max_id != 200
